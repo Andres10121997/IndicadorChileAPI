dcdf790f7e74a53ef8c2926f596e9326942a3f01
Andres10121997 <48809220+Andres10121997@users.noreply.github.com>
2025-10-22 21:22:26 -0300

    Actualización sin importancia

--- obj/Debug/net9.0/IndicadorChileAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/IndicadorChileAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Andrés Felipe Sagredo Bromberg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b3a6f26c6dfcba457f55a9923da95eb2266b0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+813a90424472e256567644f04685c8d43f7f1c55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Indicadores de Chile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IndicadorChileAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
