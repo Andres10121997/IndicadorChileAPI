48adbb99d47c37283b2d3aee44e5f5846f0eddfb
Andres10121997 <48809220+Andres10121997@users.noreply.github.com>
2025-11-07 20:42:04 -0300

    Actualización Web
    Website\Controllers\SIIController.cs
    Website\Views\Shared\_Layout.cshtml
    Website\Views\Shared\Partial\_NavBar.cshtml
    Website\Views\SII\UF.cshtml

---
+++ Website/Controllers/SIIController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Website.Controllers
+{
+    public class SIIController : Controller
+    {
+        private readonly ILogger<SIIController> Logger;
+
+
+
+        public SIIController(ILogger<SIIController> Logger)
+            : base()
+        {
+            this.Logger = Logger;
+        }
+
+
+
+        public IActionResult UF()
+        {
+            return View();
+        }
+    }
+}
