48edab28a58358d7b5c17cba3cf8d640500efb6c
Andres10121997 <48809220+Andres10121997@users.noreply.github.com>
2025-11-18 08:42:05 -0300

    Actualización
    API\App\Start\Program.cs
    docker-compose.override.yml
    docker-compose.yml

--- API/App/Start/Program.cs
+++ API/App/Start/Program.cs
@@ -2,7 +2,7 @@ using System.Threading.Tasks;
 
 namespace API.App.Start
 {
-    public class Program
+    public static class Program
     {
         public static async Task Main(string[] args)
         {
