5ac5e786f1fd8a5d1b445904a366e48354d647cc
Andres10121997 <48809220+Andres10121997@users.noreply.github.com>
2026-04-08 12:05:33 -0400

    Actualización
    App Record:
    API\App\Record\Currency\CurrencyListHeaderRecord.cs
    API\App\Record\Currency\CurrencyRecord.cs

--- API/App/Record/Currency/CurrencyListHeaderRecord.cs
+++ API/App/Record/Currency/CurrencyListHeaderRecord.cs
@@ -1,35 +1,10 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.Globalization;
 
 namespace API.App.Record.Currency
 {
     public sealed record CurrencyListHeaderRecord
     {
         #region Field
-        [
-            DataType(
-                dataType: DataType.Date,
-                ErrorMessageResourceType = typeof(DateOnly)
-            ),
-            Display(
-                AutoGenerateField = false,
-                AutoGenerateFilter = false,
-                Description = "",
-                GroupName = nameof(CurrencyListHeaderRecord),
-                Name = "Fecha de la consulta",
-                Order = 1,
-                Prompt = "Ingrese aquí la fecha de la consulta.",
-                ShortName = "Fecha consulta"
-            ),
-            Editable(
-                allowEdit: false
-            ),
-            Required(
-                AllowEmptyStrings = false,
-                ErrorMessageResourceType = typeof(DateOnly)
-            )
-        ]
         public required DateOnly ConsultationDate
         {
             get => field;
@@ -46,50 +21,8 @@ namespace API.App.Record.Currency
             }
         }
 
-        [
-            DataType(
-                dataType: DataType.Time,
-                ErrorMessageResourceType = typeof(TimeOnly)
-            ),
-            Display(
-                AutoGenerateField = false,
-                AutoGenerateFilter = false,
-                Description = "",
-                GroupName = nameof(CurrencyListHeaderRecord),
-                Name = "Hora de la consulta",
-                Order = 2,
-                Prompt = "Ingrese aquí la hora de la consulta",
-                ShortName = "Hora consulta"
-            ),
-            Editable(
-                allowEdit: false
-            ),
-            Required(
-                AllowEmptyStrings = false,
-                ErrorMessageResourceType = typeof(TimeOnly)
-            )
-        ]
         public required TimeOnly ConsultationTime { get; init; }
 
-        [
-            Display(
-                AutoGenerateField = false,
-                AutoGenerateFilter = false,
-                Description = "",
-                GroupName = nameof(CurrencyListHeaderRecord),
-                Name = "Año",
-                Order = 3,
-                Prompt = "Ingrese aquí el año.",
-                ShortName = "Año"
-            ),
-            Editable(
-                allowEdit: false
-            ),
-            Required(
-                AllowEmptyStrings = false,
-                ErrorMessageResourceType = typeof(ushort)
-            )
-        ]
         public required ushort Year
         {
             get => field;
@@ -106,85 +39,12 @@ namespace API.App.Record.Currency
             }
         }
 
-        [
-            DataType(
-                dataType: DataType.Text,
-                ErrorMessageResourceType = typeof(string)
-            ),
-            Display(
-                AutoGenerateField = false,
-                AutoGenerateFilter = false,
-                Description = "",
-                GroupName = nameof(CurrencyListHeaderRecord),
-                Name = "Nombre del mes",
-                Order = 4,
-                Prompt = "Ingrese aquí el nombre del mes.",
-                ShortName = "Mes"
-            ),
-            Editable(
-                allowEdit: false
-            ),
-            MaxLength(
-                length: 10,
-                ErrorMessageResourceType = typeof(string)
-            ),
-            MinLength(
-                length: 5,
-                ErrorMessageResourceType = typeof(string)
-            ),
-            StringLength(
-                maximumLength: 10,
-                MinimumLength = 5,
-                ErrorMessageResourceType = typeof(string)
-            )
-        ]
         public string? MonthName
         {
             get => field;
-            init
-            {
-                string[] Months = CultureInfo.CurrentCulture.DateTimeFormat.MonthNames;
-
-                if (value is not null
-                    &&
-                    !Months.Contains(value: value, StringComparer.OrdinalIgnoreCase))
-                {
-                    new ValidationResult(
-                        errorMessage: "Error",
-                        memberNames: new[]
-                        {
-                            nameof(MonthName)
-                        }
-                    );
-                }
-
-                field = value;
-            }
+            init;
         }
 
-        [
-            Display(
-                AutoGenerateField = false,
-                AutoGenerateFilter = false,
-                Description = "",
-                GroupName = nameof(CurrencyListHeaderRecord),
-                Name = "Lista de divisas",
-                Order = 5,
-                Prompt = "",
-                ShortName = "Divisas"
-            ),
-            Editable(
-                allowEdit: false
-            ),
-            MinLength(
-                length: 1,
-                ErrorMessageResourceType = typeof(CurrencyRecord[])
-            ),
-            Required(
-                AllowEmptyStrings = false,
-                ErrorMessageResourceType = typeof(CurrencyRecord[])
-            )
-        ]
         public required CurrencyRecord[] Currencies { get; init; }
         #endregion
     }
--- API/App/Record/Currency/CurrencyRecord.cs
+++ API/App/Record/Currency/CurrencyRecord.cs
@@ -1,133 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace API.App.Record.Currency
 {
-    public sealed record CurrencyRecord : IValidatableObject
+    public sealed record CurrencyRecord
     {
         #region Field
-        [
-            DataType(
-                dataType: DataType.Custom,
-                ErrorMessageResourceType = typeof(Guid)
-            ),
-            Display(
-                AutoGenerateField = false,
-                AutoGenerateFilter = false,
-                Description = "https://www.youtube.com/shorts/UwcOL3ZL3go",
-                GroupName = nameof(CurrencyRecord),
-                Name = "Identificador único",
-                Order = 1,
-                Prompt = "Ingrese el identificador único único aquí.",
-                ShortName = "ID"
-            ),
-            Editable(
-                allowEdit: false
-            ),
-            Key,
-            Required(
-                AllowEmptyStrings = false,
-                ErrorMessageResourceType = typeof(Guid)
-            )
-        ]
         public required Guid ID { get; init; }
-
-        [
-            DataType(
-                dataType: DataType.Date,
-                ErrorMessageResourceType = typeof(DateOnly)
-            ),
-            Display(
-                AutoGenerateField = false,
-                AutoGenerateFilter = false,
-                Description = "",
-                GroupName = nameof(CurrencyRecord),
-                Name = "Fecha",
-                Order = 2,
-                Prompt = "Ingrese aquí la fecha.",
-                ShortName = "Fecha"
-            ),
-            Editable(
-                allowEdit: false
-            ),
-            Required(
-                AllowEmptyStrings = false,
-                ErrorMessageResourceType = typeof(DateOnly)
-            )
-        ]
         public required DateOnly Date { get; init; }
-
-        [
-            DataType(
-                dataType: DataType.Text,
-                ErrorMessageResourceType = typeof(string)
-            ),
-            Display(
-                AutoGenerateField = false,
-                AutoGenerateFilter = false,
-                Description = "",
-                GroupName = nameof(CurrencyRecord),
-                Name = "Nombre del día de la semana",
-                Order = 3,
-                Prompt = "Ingrese aquí el nombre del día de la semana.",
-                ShortName = "Día de la semana"
-            ),
-            Editable(
-                allowEdit: false
-            ),
-            Required(
-                AllowEmptyStrings = false,
-                ErrorMessageResourceType = typeof(string)
-            )
-        ]
         public required string WeekdayName { get; init; }
-
-        [
-            DataType(
-                dataType: DataType.Currency,
-                ErrorMessageResourceType = typeof(float)
-            ),
-            Display(
-                AutoGenerateField = false,
-                AutoGenerateFilter = false,
-                Description = "",
-                GroupName = nameof(CurrencyRecord),
-                Name = "Valor divisa",
-                Order = 4,
-                Prompt = "Ingrese aquí el valor de la divisa.",
-                ShortName = "Divisa"
-            ),
-            Editable(
-                allowEdit: false
-            ),
-            Required(
-                AllowEmptyStrings = false,
-                ErrorMessageResourceType = typeof(float)
-            ),
-            Range(
-                minimum: 0,
-                maximum: float.MaxValue,
-                ErrorMessageResourceType = typeof(float)
-            )
-        ]
         public required float Currency { get; init; }
         #endregion
-
-
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (this.Currency == float.NaN)
-            {
-                yield return new ValidationResult(
-                    errorMessage: $"No puede ser {nameof(float.NaN)}",
-                    memberNames: new[]
-                    {
-                        nameof(this.Currency)
-                    }
-                );
-            }
-        }
     }
 }
