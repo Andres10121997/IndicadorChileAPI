64a1ca1f91f7e5cfce0eeeeb2b00808da5782989
Andres10121997 <48809220+Andres10121997@users.noreply.github.com>
2026-05-20 14:31:45 -0400

    Actualización
    API\App\Context\Tool\Data.cs
    API\App\Context\Tool\Extract.cs

---
+++ API/App/Context/Tool/Data.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace API.App.Context.Tool
+{
+    internal static class Data<T>
+        where T : struct, IFloatingPoint<T>
+    {
+        #region Objects
+        private static object lockObject;
+        #endregion
+
+        #region Collections
+        private static Dictionary<byte, T[]> data;
+        #endregion
+
+
+
+        #region Constructor Method
+        static Data()
+        {
+            #region Objects
+            lockObject = new object();
+            #endregion
+
+            #region Collections
+            data = new Dictionary<byte, T[]>();
+            #endregion
+        }
+        #endregion
+
+
+
+        internal static async Task<Dictionary<byte, T[]>> GetAsync(MatchCollection RowMatches)
+        {
+            await Parallel.ForEachAsync<Match>(
+                source: RowMatches,
+                parallelOptions: Utils.ParallelForEachOptions,
+                body: async (RowMatch, CancellationToken) =>
+                {
+                    #region Objects
+                    Result<MatchCollection> cellResult;
+                    #endregion
+
+                    cellResult = Extract<T>.Cell(RowMatch: RowMatch);
+
+                    if (cellResult.IsSuccess)
+                    {
+                        Parse(CellMatches: cellResult.Value);
+                    }
+                }
+            );
+
+            return data;
+        }
+
+
+
+        private static void Parse(MatchCollection CellMatches)
+        {
+            // Primera celda: el día
+            if (byte.TryParse(s: Regex.Replace(input: CellMatches[0].Groups[1].Value,
+                                               pattern: @"\D",
+                                               replacement: ""),
+                              result: out byte day))
+            {
+                Set(CellMatches: CellMatches, Day: day);
+            }
+        }
+
+        private static void Set(MatchCollection CellMatches,
+                                    byte Day)
+        {
+            #region Collections
+            T[] values;
+            #endregion
+
+            values = new T[12];
+
+            for (byte i = 1; i < CellMatches.Count; i++)
+            {
+                #region Variables
+                string value;
+                #endregion
+
+                value = CellMatches[i].Groups[1].Value
+                    .Trim()
+                    // Eliminar puntos
+                    .Replace(
+                        oldValue: ".",
+                        newValue: ""
+                    )
+                    // Cambiar comas por puntos
+                    .Replace(
+                        oldValue: ",",
+                        newValue: "."
+                    );
+
+                switch (T.TryParse(s: value,
+                                      style: NumberStyles.Number,
+                                      provider: CultureInfo.InvariantCulture,
+                                      result: out var currencyValue))
+                {
+                    case true:
+                        values[i - 1] = currencyValue;
+                        break;
+                    case false:
+                        values[i - 1] = T.Zero;
+                        break;
+                }
+            }
+
+            lock (lockObject)
+            {
+                data[Day] = values;
+            }
+        }
+    }
+}
--- API/App/Context/Tool/Extract.cs
+++ API/App/Context/Tool/Extract.cs
@@ -1,7 +1,6 @@
 ﻿using API.App.DTO;
 using API.App.DTO.HTML;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Numerics;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -11,36 +10,10 @@ namespace API.App.Context.Tool
     public static class Extract<T>
         where T : struct, IFloatingPoint<T>
     {
-        #region Objects
-        private static object lockObject;
-        #endregion
-
-        #region Collections
-        private static Dictionary<byte, T[]> data;
-        #endregion
-
-
-
         #region Constructor Method
         static Extract()
         {
-            #region Objects
-            lockObject = new object();
-            #endregion
-
-            #region Collections
-            data = new Dictionary<byte, T[]>();
-            #endregion
-        }
-        #endregion
-
-
-
-        #region Field
-        public static Dictionary<byte, T[]> Data
-        {
-            get => data;
-            set => data = value;
+            
         }
         #endregion
 
@@ -105,23 +78,7 @@ namespace API.App.Context.Tool
             Dictionary<byte, T[]> data;
             #endregion
 
-            await Parallel.ForEachAsync<Match>(
-                source: RowMatches,
-                parallelOptions: Utils.ParallelForEachOptions,
-                body: async (RowMatch, CancellationToken) =>
-                {
-                    Result<MatchCollection> cellResult;
-
-                    cellResult = Cell(RowMatch: RowMatch);
-
-                    if (cellResult.IsSuccess)
-                    {
-                        ParseData(CellMatches: cellResult.Value);
-                    }
-                }
-            );
-
-            data = Data;
+            data = await Data<T>.GetAsync(RowMatches: RowMatches);
 
             if (data.Count == 0)
             {
@@ -139,7 +96,7 @@ namespace API.App.Context.Tool
             return Result<Dictionary<byte, T[]>>.Success(Value: data);
         }
 
-        private static Result<MatchCollection> Cell(Match RowMatch)
+        public static Result<MatchCollection> Cell(Match RowMatch)
         {
             #region Variables
             string rowHtml;
@@ -187,66 +144,6 @@ namespace API.App.Context.Tool
             }
 
             return Result<MatchCollection>.Success(Value: cellMatches);
-        }
-
-        private static void ParseData(MatchCollection CellMatches)
-        {
-            // Primera celda: el día
-            if (byte.TryParse(s: Regex.Replace(input: CellMatches[0].Groups[1].Value,
-                                               pattern: @"\D",
-                                               replacement: ""),
-                              result: out byte day))
-            {
-                SetData(CellMatches: CellMatches, Day: day);
-            }
-        }
-
-        private static void SetData(MatchCollection CellMatches,
-                                    byte Day)
-        {
-            #region Collections
-            T[] values;
-            #endregion
-
-            values = new T[12];
-
-            for (byte i = 1; i < CellMatches.Count; i++)
-            {
-                #region Variables
-                string value;
-                #endregion
-
-                value = CellMatches[i].Groups[1].Value
-                    .Trim()
-                    // Eliminar puntos
-                    .Replace(
-                        oldValue: ".",
-                        newValue: ""
-                    )
-                    // Cambiar comas por puntos
-                    .Replace(
-                        oldValue: ",",
-                        newValue: "."
-                    );
-
-                switch (T.TryParse(s: value,
-                                      style: NumberStyles.Number,
-                                      provider: CultureInfo.InvariantCulture,
-                                      result: out var currencyValue))
-                {
-                    case true:
-                        values[i - 1] = currencyValue;
-                        break;
-                    case false:
-                        values[i - 1] = T.Zero;
-                        break;
-                }
-            }
-
-            lock (lockObject)
-            {
-                Data[Day] = values;
-            }
         }
     }
 }
